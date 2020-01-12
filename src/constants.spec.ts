4ef052369310e95fed7e952badc19c1a05852c6c
Warwick Stone <uozu.aho@gmail.com>
2020-01-12 15:18:09 +1100

    add units

--- src/constants.spec.ts
+++ src/constants.spec.ts
@@ -4,4 +4,8 @@ describe('constants', () => {
     it('should have 81 squares', () => {
         expect(constants.squares.length).toBe(81);
     });
+
+    it('should have 27 units', () => {
+        expect(constants.units.length).toBe(27);
+    });
 });
--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,37 @@
 export const digits  = '123456789';
 export const rows    = 'ABCDEFGHI';
 export const cols    = digits;
+/** a 'square' is a single cell of the sudoku grid */
 export const squares = cross(rows, cols);
+/** a 'unit' is a collection of 9 squares - a column, row, or box */
+export const units =
+    box_units()
+        .concat(row_units())
+        .concat(col_units());
 
-// cross product of elements in a and b
+function build_unit_lookup() {
+    
+}
+
+function col_units() {
+    return Array.from(cols).map(c => cross(c, rows));
+}
+
+function row_units() {
+    return Array.from(rows).map(r => cross(r, cols));
+}
+
+function box_units() {
+    const boxes = [];
+    for (const rs of ['ABC', 'DEF', 'GHI']) {
+        for (const cs of ['123', '456', '789']) {
+            boxes.push(cross(rs, cs))
+        }
+    }
+    return boxes;
+}
+
+// cross product of characters in a and b
 // todo: use generator
 function cross(A: string, B: string): string[] {
     const results = [];
