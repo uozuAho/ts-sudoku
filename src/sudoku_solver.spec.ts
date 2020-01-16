5e56e14cf02b9a17b6a93ec85ad541ca40f11ecc
Warwick Stone <uozu.aho@gmail.com>
2020-01-16 22:44:15 +1100

    more solver tests

--- src/sudoku_solver.spec.ts
+++ src/sudoku_solver.spec.ts
@@ -48,7 +48,7 @@ describe('SudokuSolver', () => {
         expect(SudokuSolver.solve(initial_values)).toBe(null);
     });
 
-    it('should partially solve first attempt', () => {
+    it('should solve harder puzzle', () => {
         const initial_values = ValuesMap.fromString(
             '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......');
 
@@ -64,6 +64,37 @@ describe('SudokuSolver', () => {
             '2 8 9 |6 4 3 |5 7 1 \n' +
             '5 7 3 |2 9 1 |6 8 4 \n' +
             '1 6 4 |8 7 5 |2 9 3');
+
+        const solution = SudokuSolver.solve(initial_values);
+        expect(solution.equals(expected_solution)).toBe(true);
+    });
+
+    it('should solve hardest puzzle', () => {
+        const initial_values = ValuesMap.fromString(
+            '8 5 . |. . 2 |4 . . \n' +
+            '7 2 . |. . . |. . 9 \n' +
+            '. . 4 |. . . |. . . \n' +
+            '------+------+------\n' +
+            '. . . |1 . 7 |. . 2 \n' +
+            '3 . 5 |. . . |9 . . \n' +
+            '. 4 . |. . . |. . . \n' +
+            '------+------+------\n' +
+            '. . . |. 8 . |. 7 . \n' +
+            '. 1 7 |. . . |. . . \n' +
+            '. . . |. 3 6 |. 4 .');
+
+        const expected_solution = ValuesMap.fromString(
+            '8 5 9 |6 1 2 |4 3 7 \n' +
+            '7 2 3 |8 5 4 |1 6 9 \n' +
+            '1 6 4 |3 7 9 |5 2 8 \n' +
+            '------+------+----- \n' +
+            '9 8 6 |1 4 7 |3 5 2 \n' +
+            '3 7 5 |2 6 8 |9 1 4 \n' +
+            '2 4 1 |5 9 3 |7 8 6 \n' +
+            '------+------+----- \n' +
+            '4 3 2 |9 8 1 |6 7 5 \n' +
+            '6 1 7 |4 2 5 |8 9 3 \n' +
+            '5 9 8 |7 3 6 |2 4 1 ');
 
         const solution = SudokuSolver.solve(initial_values);
         expect(solution.equals(expected_solution)).toBe(true);
