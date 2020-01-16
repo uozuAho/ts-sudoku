526745b8ab0df2f3c602974b2e64364e1a997107
Warwick Stone <uozu.aho@gmail.com>
2020-01-16 20:29:03 +1100

    add test: solves simple puzzle

--- src/puzzle.spec.ts
+++ src/puzzle.spec.ts
@@ -1,6 +1,27 @@
 import { Puzzle } from "./puzzle";
 
 describe('puzzle', () => {
+    it('should solve easy puzzle', () => {
+        const puzzle = Puzzle.fromString(
+            '003020600900305001001806400008102900700000008006708200002609500800203009005010300');
+
+        puzzle.solve();
+
+        expect(puzzle.toString()).toEqual(
+            '4 8 3 |9 2 1 |6 5 7 \n' +
+            '9 6 7 |3 4 5 |8 2 1 \n' +
+            '2 5 1 |8 7 6 |4 9 3 \n' +
+            '------+------+------\n' +
+            '5 4 8 |1 3 2 |9 7 6 \n' +
+            '7 2 9 |5 6 4 |1 3 8 \n' +
+            '1 3 6 |7 9 8 |2 4 5 \n' +
+            '------+------+------\n' +
+            '3 7 2 |6 8 9 |5 1 4 \n' +
+            '8 1 4 |2 5 3 |7 6 9 \n' +
+            '6 9 5 |4 1 7 |3 8 2 \n'
+        );
+    });
+
     it('should partially solve first attempt', () => {
         const puzzle = Puzzle.fromString(
             '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......');
